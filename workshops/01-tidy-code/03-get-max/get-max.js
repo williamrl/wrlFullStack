17e15f7dc594b45656a1ddc116d49804ef51a429
GravesBeard <68355049+GravesBeard@users.noreply.github.com>
2023-02-22 19:58:55 -0500

    tidy code

--- workshops/01-tidy-code/03-get-max/get-max.js
+++ workshops/01-tidy-code/03-get-max/get-max.js
@@ -1,2 +1,12 @@
 // YOUR CODE BELOW
-function getMax(f, s, a) {if (f > s) {if (f > a) {return f;} return a;} else if (s > a) {return s;}else {return a;}}
+function getMax(first, second, third) {
+    if (first > second) {
+        if (first > third) {
+            return first;
+        } return third;
+    } else if (second > third) {
+        return second;
+    } else {
+        return third;
+    }
+}
