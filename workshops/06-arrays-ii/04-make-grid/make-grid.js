8e60d96c59c9fdfd6ea611bce87f0c5803e0f277
Will <william.r.lawther@gmail.com>
2023-03-12 16:31:27 -0400

    arraysii

--- workshops/06-arrays-ii/04-make-grid/make-grid.js
+++ workshops/06-arrays-ii/04-make-grid/make-grid.js
@@ -1,1 +1,23 @@
 // YOUR CODE BELOW
+// Write a function `makeGrid` that accepts two arguments:
+// 1. numColumns (number)
+// 2. numRows (number)
+
+// `makeGrid` should return a two-dimensional array that represents a grid of the
+// given dimensions.
+
+// ```javascript
+// makeGrid(3,4);
+// /* => [[1, 2, 3],
+//        [1, 2, 3],
+//        [1, 2, 3],
+//        [1, 2, 3]]
+// */
+// ```
+
+const makeGrid = (numColumns, numRows) => {
+    let emptyMDA = [];
+
+    
+
+}
